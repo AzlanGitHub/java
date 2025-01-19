3625387914b123b5790fff407dcc516d95ded7db
Azlan Ainto <161608940+AzlanGitHub@users.noreply.github.com>
2025-01-19 19:17:49 +0100

    Imlementierung der Logik

--- src/main/java/org/javafx/javafxml/calculator/JRechnerController.java
+++ src/main/java/org/javafx/javafxml/calculator/JRechnerController.java
@@ -9,7 +9,7 @@ public class JRechnerController {
     @FXML private Button btnWurzeln;
     @FXML private Button btnDivision;
     @FXML private Button btnBackspace;
-
+    @FXML private Label  lblErgebnis;
 
 
     @FXML public void initialize(){
@@ -23,12 +23,52 @@ public class JRechnerController {
     }
 
     public void setSieben(ActionEvent actionEvent) {
+        lblErgebnis.setText(lblErgebnis.getText() + "7");
     }
 
     public void setVier(ActionEvent actionEvent) {
+        lblErgebnis.setText(lblErgebnis.getText() + "4");
     }
 
+
     public void setEins(ActionEvent actionEvent) {
+        if(lblErgebnis.getText().startsWith("0")){
+            lblErgebnis.setText("1");
+        }
+        else {
+            lblErgebnis.setText(lblErgebnis.getText() + "1");
+        }
+    }
+
+    public void setAcht(ActionEvent actionEvent) {
+        lblErgebnis.setText(lblErgebnis.getText() + "8");
+    }
+
+    public void setFunf(ActionEvent actionEvent) {
+        lblErgebnis.setText(lblErgebnis.getText() + "5");
+    }
+
+    public void setNeun(ActionEvent actionEvent) {
+        lblErgebnis.setText(lblErgebnis.getText() + "9");
+    }
+
+    public void setSechs(ActionEvent actionEvent) {
+        lblErgebnis.setText(lblErgebnis.getText() + "6");
+    }
+
+    public void setDrei(ActionEvent actionEvent) {
+        lblErgebnis.setText(lblErgebnis.getText() + "3");
+    }
+
+    public void setZwei(ActionEvent actionEvent) {
+        lblErgebnis.setText(lblErgebnis.getText() + "2");
+    }
+
+    public void setNull(ActionEvent actionEvent) {
+        if(lblErgebnis.getText().isEmpty() ){
+            lblErgebnis.setText("0");
+        } else
+            lblErgebnis.setText(lblErgebnis.getText() + "0");
     }
 
     public void setVorzeichen(ActionEvent actionEvent) {
@@ -37,31 +77,17 @@ public class JRechnerController {
     public void setExp(ActionEvent actionEvent) {
     }
 
-    public void setAcht(ActionEvent actionEvent) {
-    }
-
-    public void setFunf(ActionEvent actionEvent) {
-    }
-
-    public void setZwei(ActionEvent actionEvent) {
-    }
-
-    public void setNull(ActionEvent actionEvent) {
-    }
-
     public void setWurzeln(ActionEvent actionEvent) {
     }
 
-    public void setNeun(ActionEvent actionEvent) {
-    }
-
-    public void setSechs(ActionEvent actionEvent) {
-    }
-
-    public void setDrei(ActionEvent actionEvent) {
-    }
 
     public void setKomma(ActionEvent actionEvent) {
+        if(lblErgebnis.getText().endsWith(",")){
+
+        } else if (lblErgebnis.getText().contains(",")){
+
+        } else
+            lblErgebnis.setText(lblErgebnis.getText() +",");
     }
 
     public void setDivision(ActionEvent actionEvent) {
@@ -74,6 +100,20 @@ public class JRechnerController {
     }
 
     public void setPlus(ActionEvent actionEvent) {
+        String currentText = lblErgebnis.getText();
+
+        // Wenn der Text leer ist oder bereits ein Plus am Ende steht, dann nichts tun
+        if (currentText.length() == 0 || currentText.endsWith("+")) {
+            return;
+        }
+
+        // Wenn das letzte Zeichen eine Rechenoperation ist, auch nichts tun
+        if (currentText.endsWith("-") || currentText.endsWith("x") || currentText.endsWith("\u00F7")) {
+            return;
+        }
+
+        // Ansonsten das Pluszeichen hinzufügen
+        lblErgebnis.setText(currentText + "+");
     }
 
     public void getErgebnis(ActionEvent actionEvent) {
@@ -83,12 +123,42 @@ public class JRechnerController {
     }
 
     public void setClearEntry(ActionEvent actionEvent) {
+        // Wenn lblErgebnis kein Rechenzeichen enthält
+        String currentText = lblErgebnis.getText();
+        Boolean hatPlus = currentText.contains("+");
+        Boolean hatMinus = currentText.contains("-");
+        Boolean hatMal = currentText.contains("x");
+        Boolean hatDiv = currentText.contains("\u00F7");
+
+        if (!(hatPlus || hatMinus || hatMal || hatDiv)) {
+            // Löschen des gesamten Textes, wenn keine Rechenoperation vorhanden ist
+            lblErgebnis.setText("");
+        } else {
+            // Entferne die letzte Zahl
+            int lastIndex = currentText.length() - 1;
+
+            // Rückwärts durch den Text gehen und die letzte Zahl finden
+            while (lastIndex >= 0 && Character.isDigit(currentText.charAt(lastIndex))) {
+                lastIndex--;
+            }
+
+            // Setze den Text bis zur Stelle, an der die Zahl endet
+            lblErgebnis.setText(currentText.substring(0, lastIndex + 1));
+        }
     }
 
+
     public void setClearAll(ActionEvent actionEvent) {
+        lblErgebnis.setText("");
     }
 
     public void setBackspace(ActionEvent actionEvent) {
+
+        if(lblErgebnis.getText().length() > 0){
+            lblErgebnis.setText(lblErgebnis.getText().substring(0, lblErgebnis.getText().length()-1));
+
+        }
+
         
     }
 }
